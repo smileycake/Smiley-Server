fbbe3477928e5b06d42a1e7daa52297de10aa505
Taco <akataco@outlook.com>
2019-02-17 15:39:49 +0800

    first commit

---
+++ src/main/java/com/smiley/Smiley/Admin/Server/SmileyAdminServerApplication.java
@@ -0,0 +1,13 @@
+package com.smiley.Smiley.Admin.Server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmileyAdminServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmileyAdminServerApplication.class, args);
+	}
+
+}
